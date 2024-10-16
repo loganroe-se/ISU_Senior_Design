00e556bc65d5a78f4df4a1fd6e9c9bdb30ac8e6d
Zachary <zdfoote@iastate.edu>
2024-10-15 21:16:39 -0500

    made post one universal class

--- website/dripdrop/src/pages/Home.tsx
+++ website/dripdrop/src/pages/Home.tsx
@@ -1,36 +1,58 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Sidebar  from '../components/Sidebar';
-import Post1 from '../components/Post1';
-import Post2 from '../components/Post2';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Sidebar from "../components/Sidebar";
+import PostCard from "../components/PostCard";
 
 const drawerWidth = 240;
 
 export default function Home() {
+  const posts = [
+    {
+      image: "/outfit_1.jpg",
+      username: "EliseTravers",
+      caption: "New fall outfit!",
+    },
+    {
+      image: "/outfit_2.jpeg",
+      username: "JohnDoe",
+      caption: "Ready for winter!",
+    },
+    {
+      image: "/outfit_1.jpg",
+      username: "JaneSmith",
+      caption: "Summer vibes!",
+    },
+  ];
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            
-            <Container
-                component="main"
-                sx={{
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
 
-                    justifyContent: 'center',
-                    flexGrow: 1,
-                    p: 3,
-                    mt: 8
-                    
-                }}
-            >
-                <Post1/>
-                <Post2/>
-            </Container>
-        </Box>
-    );
+      <Container
+        component="main"
+        sx={{
+          justifyContent: "center",
+          flexGrow: 1,
+          p: 3,
+          mt: 8,
+        }}
+      >
+        <div className="post">
+          {posts.map((post, index) => (
+            <PostCard
+              key={index}
+              image={post.image}
+              username={post.username}
+              caption={post.caption}
+            />
+          ))}
+        </div>
+      </Container>
+    </Box>
+  );
 }
