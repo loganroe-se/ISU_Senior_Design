88685b5a269c8060e86b60f20c133b9193d826e4
KadenWingert1 <swiperkcw@gmail.com>
2024-10-17 11:46:44 -0500

    reverting. Pipeline works

--- website/dripdrop/src/components/Sidebar.tsx
+++ website/dripdrop/src/components/Sidebar.tsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
             <List>
                 <ListHeader>
                     <Typography variant="h1" fontSize={"2rem"}>
-                        Test
+                        dripdrop
                     </Typography>
                 </ListHeader>
                 {['Home', 'Search', 'Post', 'Profile', 'Settings'].map((text, index) => (
