2facbae4d7c79cf6a64c90caea4f644db7d6946c
KadenWingert1 <swiperkcw@gmail.com>
2024-10-17 23:50:04 -0500

    fixing path

--- aws/lib/constructs/static-site.ts
+++ aws/lib/constructs/static-site.ts
@@ -113,7 +113,7 @@ export class StaticSite extends Construct {
 
     // Deploy site contents to S3 bucket
     new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
-      sources: [s3deploy.Source.asset("/build")],
+      sources: [s3deploy.Source.asset("../../../website/dripdrop/build")],
       destinationBucket: siteBucket,
       distribution,
       distributionPaths: ["/*"],
