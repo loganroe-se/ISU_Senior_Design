38ecfcc9808bd7ddeb191486132bd559acf7f7e3
Elyse Kriegel <esamkriegel@gmail.com>
2024-10-30 21:29:33 -0500

    fix unused sutff

--- website/dripdrop/src/App.tsx
+++ website/dripdrop/src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import HomePage from './pages/Layout';
-import SignIn from './pages/signIn';
+// import SignIn from './pages/signIn';
 import Box from '@mui/material/Box';
 import './styles/app.css';
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
-  const handleSignIn = () => {
-    // This is where we can handle authentication logic
-    setIsSignedIn(true);
-  };
+  // const handleSignIn = () => {
+  //   // This is where we can handle authentication logic
+  //   setIsSignedIn(true);
+  // };
 
   return (
     <Box sx={{
