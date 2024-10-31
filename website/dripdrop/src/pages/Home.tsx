4fb3e4e78414885b67b20f09440c8fddadbb47f7
Elyse Kriegel <esamkriegel@gmail.com>
2024-10-31 04:05:30 -0500

    ddd

--- website/dripdrop/src/pages/Home.tsx
+++ website/dripdrop/src/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const posts = [
     {
       image: "/outfit_1.jpg",
-      username: "This is a test branch",
+      username: "This is a test brancdh",
       caption: "New fall outfit!",
     },
     {
