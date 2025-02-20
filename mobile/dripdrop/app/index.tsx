2e86c55b1074d6b6697b2ac72550b8ae17a26b1d
Zachary <zdfoote@iastate.edu>
2025-02-20 14:07:53 -0600

    cleaned code

--- mobile/dripdrop/app/index.tsx
+++ mobile/dripdrop/app/index.tsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-import { Button } from 'react-native-paper';
 import { useRouter } from 'expo-router';
-import Login from './Login';
 import SignUpScreen from './Signup';
-//import { ReadableStream } from 'web-streams-polyfill/ponyfill';
 import "react-native-get-random-values";
 import "react-native-url-polyfill/auto";
-//globalThis.ReadableStream = ReadableStream;
 
 
 export default function Index() {
@@ -16,9 +12,8 @@ export default function Index() {
 
   // Handle successful sign-up
   const onSuccessfulSignUp = () => {
-    // You can add logic to handle what happens when the sign-up is successful
     console.log("Sign up successful!");
-    router.push('/Login'); // Navigate to another page, for example, home
+    router.push('/Login'); // Navigate Login page on successful signup
   };
 
   return (
