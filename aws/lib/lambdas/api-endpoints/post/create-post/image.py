6b7176a4685c05a18d632a8e34d77a631519ee74
Kolby Kucera <kolbykuc@iastate.edu>
2025-03-10 17:19:12 -0500

    Revert AI stuff

--- aws/lib/lambdas/api-endpoints/post/create-post/image.py
+++ aws/lib/lambdas/api-endpoints/post/create-post/image.py
@@ -5,8 +5,6 @@ from dripdrop_orm_objects import Image, Post
 import base64
 import boto3
 import uuid
-import json
-import os
 
 # S3 configuration
 S3_BUCKET = "imageoptimizationstack-s3dripdroporiginalimagebuck-m18zpwypjbuc"
@@ -14,7 +12,6 @@ S3_REGION = "us-east-1"
 
 
 s3_client = boto3.client("s3", region_name=S3_REGION)
-sqs_client = boto3.client("sqs", region_name=S3_REGION)
 
 
 def save_image_to_db(session, post_id, images):
@@ -47,41 +44,5 @@ def save_image_to_db(session, post_id, images):
         # Construct the return message
         print(f"Image with imageID: {new_image.imageID} was created successfully")
 
-        # Send the image to AI processing
-        send_image_to_queue(s3_key, post_id)
-
-    # Return message
+        # Return message
     return 201, "Images successfully saved to db"
-
-
-# Helper method to send images to the queue for AI processing
-def send_image_to_queue(s3_key, post_id):
-    
-    queue_url = os.environ.get("QUEUE_URL")
-
-    if not queue_url:
-        raise Exception("500", "Queue URL environment variable not set")
-    
-    # Create message body
-    message_body = {
-        "image_path": s3_key,
-        "post_id": post_id
-    }
-
-    # Send message to the queue
-    try:
-        response = sqs_client.send_message(
-            QueueUrl=queue_url,
-            MessageBody=json.dumps(message_body),
-            MessageAttributes={
-                "EventType": {
-                    "StringValue": "Transaction",
-                    "DataType": "String"
-                }
-            }
-        )
-        
-    except Exception as e:
-        raise Exception("500", f"Error sending message to queue: {str(e)}")
-
-
