62c1e9edfa259ed7aeb4d3ed0d8a96008907a9ce
Kolby Kucera <kolbykuc@iastate.edu>
2025-04-28 16:17:26 -0500

    Fixes

--- aws/lib/lambdas/api-endpoints/user/search-users/handler.py
+++ aws/lib/lambdas/api-endpoints/user/search-users/handler.py
@@ -1,17 +1,18 @@
 import json
 from utils import create_response, handle_exception
 from sqlalchemy import select
-from sqlalchemy_utils import session_handler
+from sqlalchemy_utils import session_handler, get_user_by_email
 from dripdrop_orm_objects import User
 
 def handler(event, context):
     try:
         search_string = event['pathParameters'].get('searchString')
+        email = event['requestContext']['authorizer']['claims']['email']
 
         if not search_string:
             return create_response(400, 'Missing searchString')
 
-        status_code, message = searchUsers(search_string)
+        status_code, message = searchUsers(search_string, email)
         return create_response(status_code, message)
 
     except Exception as e:
@@ -19,11 +20,16 @@ def handler(event, context):
 
 
 @session_handler
-def searchUsers(session, search_string):
+def searchUsers(session, search_string, email):
     try:
+        user = get_user_by_email(session, email)
+
         users = session.execute(
             select(User.username, User.uuid, User.profilePicURL)
-            .filter(User.username.ilike(f"%{search_string}%"))
+            .filter(
+                User.username.ilike(f"%{search_string}%"),
+                User.userID != user.userID,
+            )
             .limit(20)
         ).fetchall()
 
